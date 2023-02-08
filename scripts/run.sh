11b87f6cdbfea82f536fa20f859c2572c839de7b
Cai Xinyu <cxyvlf@gmail.com>
2023-02-08 13:47:24 +0800

    fix hyper parameters

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 . scripts/run_$1.sh 1 no_aug 0 23 &
 . scripts/run_$1.sh 1 no_aug 1 57 &
 . scripts/run_$1.sh 1 no_aug 2 89 &
-. scripts/run_$1.sh 1 no_aug 3 111 &
+. scripts/run_$1.sh 1 no_aug 3 111
 
