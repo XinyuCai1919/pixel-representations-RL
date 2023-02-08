a8e4a1e683e0ec18d28add9abdf0e2cef9bfaf31
Cai Xinyu <cxyvlf@gmail.com>
2023-02-08 13:44:35 +0800

    fix hyper parameters

---
+++ scripts/run_six_env.sh
@@ -0,0 +1,12 @@
+. scripts/run.sh cartpole
+. scripts/run.sh cheetah
+. scripts/run.sh finger
+. scripts/run.sh hopper
+. scripts/run.sh reacher
+. scripts/run.sh walker
+
+
+
+
+
+
---
+++ scripts/run_six_env_aug.sh
@@ -0,0 +1,12 @@
+. scripts/run_aug.sh cartpole
+. scripts/run_aug.sh cheetah
+. scripts/run_aug.sh finger
+. scripts/run_aug.sh hopper
+. scripts/run_aug.sh reacher
+. scripts/run_aug.sh walker
+
+
+
+
+
+
