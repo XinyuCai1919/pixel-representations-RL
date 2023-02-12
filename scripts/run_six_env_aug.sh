3667babf360e8460ca494c60c23907a4f18979d3
xinyu.cai <cxyvlf@gmail.com>
2023-02-12 14:35:41 +0800

    fix sh

--- scripts/run_six_env_aug.sh
+++ scripts/run_six_env_aug.sh
@@ -1,9 +1,9 @@
-. scripts/run_aug.sh cartpole 1 2 3 3
-. scripts/run_aug.sh cheetah 1 2 3 3
-. scripts/run_aug.sh finger 1 2 3 3
-. scripts/run_aug.sh hopper 1 2 3 3
-. scripts/run_aug.sh reacher 1 2 3 3
-. scripts/run_aug.sh walker 1 2 3 3
+. scripts/run_aug.sh cartpole 0 1 2 3
+. scripts/run_aug.sh cheetah 0 1 2 3
+. scripts/run_aug.sh finger 0 1 2 3
+. scripts/run_aug.sh hopper 0 1 2 3
+. scripts/run_aug.sh reacher 0 1 2 3
+. scripts/run_aug.sh walker 0 1 2 3
 
 
 
