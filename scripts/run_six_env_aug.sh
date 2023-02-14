d3a5579f85a80894d75fe364abaed14f5ee3daea
xinyu.cai <cxyvlf@gmail.com>
2023-02-14 13:47:37 +0800

    eval frequ

--- scripts/run_six_env_aug.sh
+++ scripts/run_six_env_aug.sh
@@ -1,5 +1,5 @@
-. scripts/run_aug.sh cartpole 0 1 2 3
-. scripts/run_aug.sh cheetah 0 1 2 3
+#. scripts/run_aug.sh cartpole 0 1 2 3
+#. scripts/run_aug.sh cheetah 0 1 2 3
 . scripts/run_aug.sh finger 0 1 2 3
 . scripts/run_aug.sh hopper 0 1 2 3
 . scripts/run_aug.sh reacher 0 1 2 3
