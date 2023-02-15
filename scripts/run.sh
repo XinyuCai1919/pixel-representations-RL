1a60ba84c0c66ae523675915e367ce9dd477156a
xinyu.cai <cxyvlf@gmail.com>
2023-02-15 14:09:16 +0800

    run 4 &

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 . scripts/run_$1.sh 1 no_aug $2 23 &
 . scripts/run_$1.sh 1 no_aug $3 57 &
 . scripts/run_$1.sh 1 no_aug $4 89 &
-. scripts/run_$1.sh 1 no_aug $5 111
+. scripts/run_$1.sh 1 no_aug $5 111 &
 
