9bada37da12b6d0172296bd0b1a87bfe266bd13e
Xinyu Cai <cxyvlf@gmail.com>
2023-02-24 14:50:40 +0800

    CtrFormer

--- scripts/docker_run_example.sh
+++ scripts/docker_run_example.sh
@@ -2,6 +2,6 @@ docker run -v /home/xycai/pixel-representations-RL:/opt/project --runtime=nvidia
             -e NVIDIA_DRIVER_CAPABILITIES=compute,utility -e NVIDIA_VISIBLE_DEVICES=all \
             -w /opt/project -it --rm cxy/py38:0.2  bash
 
-docker run -v /home/xycai/pixel-representations-RL:/opt/project --gpus==all --cpus==40\
+docker run -v /home/xycai/pixel-representations-RL:/opt/project --gpus=all --cpus=40\
             -w /opt/project -it --rm cxy/py38:0.2  bash
 
