347496612b6294ba2bc9d887836e7a11dafeabc1
Xinyu Cai <cxyvlf@gmail.com>
2023-02-24 14:51:18 +0800

    CtrFormer

--- ViT4RL/run.sh
+++ ViT4RL/run.sh
@@ -1,5 +1,5 @@
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=0 python train.py  batch_size=256 action_repeat=8 env=cartpole_swingup seed=23 &
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=1 python train.py  batch_size=256 action_repeat=8 env=cartpole_swingup seed=57 &
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=2 python train.py  batch_size=256 action_repeat=8 env=cartpole_swingup seed=89 &
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=3 python train.py  batch_size=256 action_repeat=8 env=cartpole_swingup seed=111
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=0 python train.py  batch_size=512 action_repeat=8 env=cartpole_swingup seed=23 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=1 python train.py  batch_size=512 action_repeat=8 env=cartpole_swingup seed=57 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=2 python train.py  batch_size=512 action_repeat=8 env=cartpole_swingup seed=89 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=3 python train.py  batch_size=512 action_repeat=8 env=cartpole_swingup seed=111
 
