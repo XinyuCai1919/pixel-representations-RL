e317b6a1affc6af0d5be2cd89361d9f0d4d593e7
xinyu <cxyvlf@gmail.com>
2023-04-13 10:59:44 +0800

    cheetah lr

--- ViT4RL/run_cheetah.sh
+++ ViT4RL/run_cheetah.sh
@@ -1,5 +1,5 @@
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=0 python train.py  batch_size=512 lr=1e-3 action_repeat=4 env=cheetah_run seed=23 &
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=1 python train.py  batch_size=512 lr=1e-3 action_repeat=4 env=cheetah_run seed=57 &
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=2 python train.py  batch_size=512 lr=5e-4 action_repeat=4 env=cheetah_run seed=89 &
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=3 python train.py  batch_size=512 lr=5e-4 action_repeat=4 env=cheetah_run seed=111
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=0 python train.py  batch_size=512 action_repeat=4 env=cheetah_run seed=23 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=1 python train.py  batch_size=512 action_repeat=4 env=cheetah_run seed=57 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=2 python train.py  batch_size=512 action_repeat=4 env=cheetah_run seed=89 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=3 python train.py  batch_size=512 action_repeat=4 env=cheetah_run seed=111
 
