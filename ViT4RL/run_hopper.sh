7d05bc87dd79c1a8525836733a9f8b689804ae4c
xinyu <cxyvlf@gmail.com>
2023-04-17 10:41:12 +0800

    add more env instant

--- ViT4RL/run_hopper.sh
+++ ViT4RL/run_hopper.sh
@@ -1,5 +1,5 @@
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=0 python train.py  batch_size=512 action_repeat=4 env=hopper_stand seed=23 &
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=1 python train.py  batch_size=512 action_repeat=4 env=hopper_stand seed=57 &
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=2 python train.py  batch_size=512 action_repeat=4 env=hopper_stand seed=89 &
-MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=3 python train.py  batch_size=512 action_repeat=4 env=hopper_stand seed=111
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=0 python train.py  batch_size=512 action_repeat=4 env=hopper_hop seed=23 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=1 python train.py  batch_size=512 action_repeat=4 env=hopper_hop seed=57 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=2 python train.py  batch_size=512 action_repeat=4 env=hopper_hop seed=89 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=3 python train.py  batch_size=512 action_repeat=4 env=hopper_hop seed=111
 
