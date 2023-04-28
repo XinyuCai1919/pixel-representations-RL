a6537f013b0292b579b2ec841ba17117880a9e38
xinyu <cxyvlf@gmail.com>
2023-04-28 11:39:27 +0800

    add exp script

---
+++ ViT4RL/run_hopper.sh
@@ -0,0 +1,4 @@
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=0 python train.py  batch_size=512 action_repeat=4 env=hopper_stand seed=23 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=1 python train.py  batch_size=512 action_repeat=4 env=hopper_stand seed=57 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=2 python train.py  batch_size=512 action_repeat=4 env=hopper_stand seed=89 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=3 python train.py  batch_size=512 action_repeat=4 env=hopper_stand seed=111
