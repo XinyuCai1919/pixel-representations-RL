907f50d81c8722f28847c375273f0983628eb3e4
xinyu <cxyvlf@gmail.com>
2023-05-07 15:26:29 +0800

    run sh

---
+++ ViT4RL/run_walker_stand.sh
@@ -0,0 +1,2 @@
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=$1 python train.py  batch_size=256 action_repeat=4 env=walker_stand seed=23 &
+MUJOCO_GL="osmesa" CUDA_VISIBLE_DEVICES=$2 python train.py  batch_size=256 action_repeat=4 env=walker_stand seed=57 &
