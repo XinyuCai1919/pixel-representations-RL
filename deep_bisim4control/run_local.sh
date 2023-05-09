12a0596594e034a0d77d13d4078b804a28c44a5e
xinyu <cxyvlf@gmail.com>
2023-05-09 23:38:29 +0800

    batch size

--- deep_bisim4control/run_local.sh
+++ deep_bisim4control/run_local.sh
@@ -2,15 +2,16 @@
 
 DOMAIN=$1
 TASK=$2
-SEED=$3
-CUDA=$4
+REPEAT=$3
+SEED=$4
+CUDA=$5
 
 SAVEDIR=./save
 
 MUJOCO_GL="osmesa"  CUDA_VISIBLE_DEVICES=${CUDA} python train.py \
     --domain_name ${DOMAIN} \
     --task_name ${TASK} \
-    --action_repeat 2 \
+    --action_repeat ${REPEAT} \
     --save_tb \
     --save_model \
     --work_dir ${SAVEDIR}/${DOMAIN}_${TASK}_${SEED} \
