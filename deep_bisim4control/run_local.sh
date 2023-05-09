92764cc64c093cc1d50b5f815db3837dec8d7b05
xinyu <cxyvlf@gmail.com>
2023-05-09 10:35:11 +0800

    path

--- deep_bisim4control/run_local.sh
+++ deep_bisim4control/run_local.sh
@@ -16,5 +16,3 @@ MUJOCO_GL="osmesa"  CUDA_VISIBLE_DEVICES=${CUDA} python train.py \
     --save_model \
     --work_dir ${SAVEDIR}/${DOMAIN}_${TASK}_${SEED} \
     --seed ${SEED} &
-
-. run_local.sh cartpole swingup 23 0
